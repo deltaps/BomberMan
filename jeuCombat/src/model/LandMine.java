104cba9f01e7713072389c84c61f9212b1fe5d58
Sacha <sacha.pronost@gmail.com>
2021-10-22 12:58:20 +0200

    Changes by Mathi

--- jeuCombat/src/model/LandMine.java
+++ jeuCombat/src/model/LandMine.java
@@ -4,16 +4,14 @@ import personnagesJeu.Personnage;
 
 public class LandMine implements Weapon {
 
-    int x;
-    int y;
+    protected int[] position;
     protected Personnage owner;
     protected Plateau plateau;
 
-    public void detonation(Personnage owner, Plateau plateau, int x, int y) {
+    public void detonation(Personnage owner, int[] position, Plateau plateau) {
         this.owner = owner;
+        this.position = position;
         this.plateau = plateau;
-        this.x = x;
-        this.y = y;
     }
 
     public Personnage getOwner() {
