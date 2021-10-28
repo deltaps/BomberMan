41fe97393a7fde392cf4b08bd59603d2838ad70c
Sacha <sacha.pronost@gmail.com>
2021-10-28 17:53:10 +0200

    Changes by Mathi

---
+++ jeuCombat/src/vue/AdapterFromConcretePlateauToTableModel.java
@@ -0,0 +1,35 @@
+package vue;
+
+import model.ConcretePlateau;
+import observer.ModelListener;
+
+import javax.swing.table.AbstractTableModel;
+
+public class AdapterFromConcretePlateauToTableModel extends AbstractTableModel implements ModelListener {
+
+    public ConcretePlateau concretePlateau;
+
+    public AdapterFromConcretePlateauToTableModel(ConcretePlateau concretePlateau) {
+        this.concretePlateau = concretePlateau;
+    }
+
+    @Override
+    public int getRowCount() {
+        return this.concretePlateau.getTaille();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return this.concretePlateau.getTaille();
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        return this.concretePlateau.getCase(rowIndex, columnIndex);
+    }
+
+    @Override
+    public void somethingHasChanged(Object source) {
+        fireTableDataChanged();
+    }
+}
