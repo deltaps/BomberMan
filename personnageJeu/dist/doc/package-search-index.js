d20f07f8ba285ee940f6da7f3d0fa9d09fd96987
Sacha Pronost <21901956@etu.unicaen.fr>
2021-12-08 10:48:18 +0100

    Préparation pour le rendu

--- personnageJeu/dist/doc/package-search-index.js
+++ personnageJeu/dist/doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"personnagesJeu"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"personnagesJeu"}];updateSearchResults();
